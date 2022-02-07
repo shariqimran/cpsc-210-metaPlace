e5fafdb29d43bd3a52d803623c9a06ed86306776
Shariq <shariqimranhassan@icloud.com>
2022-02-06 16:11:29 -0800

    Created Classes

---
+++ src/main/model/Account.java
@@ -0,0 +1,9 @@
+package model;
+
+//Add funds to your account so you can purchase desired product
+//Gives cashback rewards
+//Can buy product by negotiating
+//Can see your purchases
+
+public class Account {
+}
---
+++ src/main/model/Product.java
@@ -0,0 +1,9 @@
+package model;
+
+//Has all the products in the marketplace
+//Has product descriptions and prices
+//Has product specific discount?(Lowest seller would go for said product)
+//Can list your own product with specific price and lowest you would go
+
+public class Product {
+}
